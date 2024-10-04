47b2961fa0d7aeb048c8f51519659fe7c3a51721
Jhon P <edip9814@gmail.com>
2024-10-04 10:47:50 -0500

    Practica de VUE

---
+++ vueTerminal/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hola mundo</h1>
+    
+    <MyCounter :value="5"/>
+</template>
+
+<script lang="ts" setup>
+    import MyCounter from './Components/MyCounter.vue';
+</script>
+
+<style scoped>
+h1{
+    color: blue;
+}
+</style>
---
+++ vueTerminal/src/Components/MyCounter.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hola mundo</h1>
+    <h3>contador: {{ contador }}</h3>
+    <h3>Cuadrado: {{ contador*contador }}</h3>
+
+    <div>
+        <button @click="contador++">+1</button>
+        <button @click="contador--">-1</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import{ref} from 'vue';
+
+    const props = defineProps<{
+        value: number;
+    }>();
+    
+    const contador = ref(props.value);
+    
+</script>
